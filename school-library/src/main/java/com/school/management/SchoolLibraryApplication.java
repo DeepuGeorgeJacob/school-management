c0cd54f318af11c90dfe3324c61ae4be86a3046f
Deepu George Jacob <deepugeorgejacob@Deepus-MacBook-Pro.local>
2022-10-31 13:54:18 +0100

    Eureka server

--- school-library/src/main/java/com/school/management/SchoolLibraryApplication.java
+++ school-library/src/main/java/com/school/management/SchoolLibraryApplication.java
@@ -3,12 +3,14 @@ package com.school.management;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class SchoolLibraryApplication {
     @Value("${student.service.url}")
     private String studentUrl;
