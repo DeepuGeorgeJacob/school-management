358e1307ecfb2d3e8b60e9a47eaf7fcdf8dafedf
Deepu George Jacob <deepugeorgejacob@Deepus-MacBook-Pro.local>
2023-01-30 00:36:28 +0100

    Dynamic propert changes in progress

--- dynamic-config-change/src/main/java/com/school/management/controller/GreetingController.java
+++ dynamic-config-change/src/main/java/com/school/management/controller/GreetingController.java
@@ -1,9 +1,12 @@
 package com.school.management.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RefreshScope
 @RestController
 public class GreetingController {
 
