b8f044b55a269d20835ce06dcd7e961a0523133b
Deepu George Jacob <deepugeorgejacob@Deepus-MacBook-Pro.local>
2023-06-16 11:43:04 +0200

    New  module update

--- SqlNative/src/main/java/com/school/management/SqlNativeApplication.java
+++ SqlNative/src/main/java/com/school/management/SqlNativeApplication.java
@@ -1,2 +1,11 @@
-package com.school.management;public class SqlNativeApplication {
+package com.school.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlNativeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SqlNativeApplication.class);
+    }
 }
--- SqlNative/src/main/java/com/school/management/controller/NativeQueryController.java
+++ SqlNative/src/main/java/com/school/management/controller/NativeQueryController.java
@@ -1,2 +1,13 @@
-package com.school.management.controller;public class NativeQueryController {
+package com.school.management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NativeQueryController {
+
+    @GetMapping(value = "/hello")
+    public String greetings() {
+        return "Hello world";
+    }
 }
